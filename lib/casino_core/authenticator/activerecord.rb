d06dd8c61484ace59906d05a348faa30ef4432c0
Raffael Schmid <raf@nine.ch>
2013-02-19 14:52:47 +0100

    add tests and code for authenticating against a database

--- lib/casino_core/authenticator/activerecord.rb
+++ lib/casino_core/authenticator/activerecord.rb
@@ -1,15 +1,38 @@
 require 'casino_core/authenticator'
+require 'active_record'
+require 'unix_crypt'
 
 class CASinoCore::Authenticator::ActiveRecord
 
   # @param [Hash] options
   def initialize(options)
     @options = options
+    ::ActiveRecord::Base.establish_connection @options[:connection]
+
+    eval <<-END
+      class #{@options[:table].classify} < ActiveRecord::Base
+      end
+    END
+
+    @model = "#{self.class.to_s}::#{@options[:table].classify}".constantize
   end
 
   def validate(username, password)
-    @username = username
-    @password = password
-    nil
+    user = @model.send("find_by_#{@options[:username_column]}!", username)
+    password_from_database = user.send(@options[:password_column])
+
+    if valid_password?(password, password_from_database)
+      { username: user.send(@options[:username_column]) }
+    else
+      false
+    end
+
+  rescue ActiveRecord::RecordNotFound
+    false
+  end
+
+  private
+  def valid_password?(password, password_from_database)
+    UnixCrypt.valid?(password, password_from_database)
   end
 end
--- spec/casino_core/authenticator/activerecord_spec.rb
+++ spec/casino_core/authenticator/activerecord_spec.rb
@@ -2,4 +2,57 @@ require 'spec_helper'
 require 'casino_core/authenticator/activerecord'
 
 describe CASinoCore::Authenticator::ActiveRecord do
+
+  let(:options) do
+    {
+      connection: {
+        adapter: 'sqlite3',
+        database: ':memory:'
+      },
+      table: 'users',
+      username_column: 'username',
+      password_column: 'password'
+    }
+  end
+
+  before do
+
+    @authenticator = CASinoCore::Authenticator::ActiveRecord.new(options)
+
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Schema.define do
+        create_table :users do |t|
+          t.string :username
+          t.string :password
+        end
+      end
+    end
+
+    CASinoCore::Authenticator::ActiveRecord::User.create!(username: 'test', password: '$5$cegeasjoos$vPX5AwDqOTGocGjehr7k1IYp6Kt.U4FmMUa.1l6NrzD') # password: testpassword
+  end
+
+  describe "#validate" do
+
+    context "valid username" do
+      context "valid password" do
+        it 'returns the userdata' do
+          @authenticator.validate('test', 'testpassword').should eq({username: 'test'})
+        end
+      end
+
+      context "invalid password" do
+        it 'returns false' do
+          @authenticator.validate('test', 'wrongpassword').should eq(false)
+        end
+      end
+    end
+
+    context "invalid username" do
+      it 'returns false' do
+        @authenticator.validate('does-not-exist', 'testpassword').should eq(false)
+      end
+    end
+
+  end
+
 end
