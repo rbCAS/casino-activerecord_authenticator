b2f470bf82a3f029fed563f19dbf22205db6a787
Raman Buttar <raman@Ramans-MacBook-Pro.local>
2016-03-23 14:41:56 -0700

    check user record against additional constraint columns

--- spec/casino_core/activerecord_authenticator_spec.rb
+++ spec/casino_core/activerecord_authenticator_spec.rb
@@ -18,8 +18,18 @@ describe CASino::ActiveRecordAuthenticator do
       extra_attributes: extra_attributes
     }
   end
+
+  let(:constraint_columns) do
+    {
+        constraint_columns: {
+            is_active: true
+        }
+    }
+  end
+
   let(:faulty_options){ options.merge(table: nil) }
   let(:connection_as_string) { options.merge(connection: 'sqlite3:/tmp/casino-test-auth.sqlite') }
+  let(:constraint_columns_options){ options.merge(constraint_columns) }
   let(:user_class) { described_class::TmpcasinotestauthsqliteUser }
 
   subject { described_class.new(options) }
@@ -34,6 +44,7 @@ describe CASino::ActiveRecordAuthenticator do
         create_table :users do |t|
           t.string :username
           t.string :password
+          t.string :is_active
           t.string :mail_address
         end
       end
@@ -230,6 +241,64 @@ describe CASino::ActiveRecordAuthenticator do
       end
     end
 
+    context 'support for additional_constraint_column' do
+      subject { described_class.new(constraint_columns_options) }
+
+      context 'when is_active is not present' do
+        before do
+          user_class.create!(
+              username: 'test6',
+              password: 'testpassword6',
+              mail_address: 'mail@example.org')
+        end
+
+        it 'is able to take additional_constraint_column into consideration' do
+          subject.validate('test6', 'testpassword6').should eq(false)
+        end
+      end
+
+      context 'when is_active is nil' do
+        before do
+          user_class.create!(
+              username: 'test6',
+              password: 'testpassword6',
+              is_active: nil,
+              mail_address: 'mail@example.org')
+        end
+
+        it 'is able to take additional_constraint_column into consideration' do
+          subject.validate('test6', 'testpassword6').should eq(false)
+        end
+      end
+
+      context 'when is_active is true' do
+        before do
+          user_class.create!(
+              username: 'test6',
+              password: 'testpassword6',
+              is_active: true,
+              mail_address: 'mail@example.org')
+        end
+
+        it 'is able to take additional_constraint_column into consideration' do
+          subject.validate('test6', 'testpassword6').should be_instance_of(Hash)
+        end
+      end
+
+      context 'when is_active is false' do
+        before do
+          user_class.create!(
+              username: 'test6',
+              password: 'testpassword6',
+              is_active: false,
+              mail_address: 'mail@example.org')
+        end
+
+        it 'is able to take additional_constraint_column into consideration' do
+          subject.validate('test6', 'testpassword6').should eq(false)
+        end
+      end
+    end
   end
 
 end
